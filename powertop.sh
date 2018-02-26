015f4207cdc0a8e26d8f18a2d62afd1e1db524e5
Piotr Szotkowski <chastell@chastell.net>
2018-02-26 21:40:19 +0100

    Bash: tune that power, top!

--- powertop.sh
+++ powertop.sh
@@ -1,9 +1,13 @@
-sudo sh -c "echo 1 > /sys/module/snd_hda_intel/parameters/power_save"
+#!/bin/bash
+
+sudo sh -c "echo 1500 > /proc/sys/vm/dirty_writeback_centisecs"
+
+for device in /sys/bus/i2c/devices/*/device /sys/bus/pci/devices/*; do
+  sudo sh -c "echo auto > $device/power/control"
+done
 
 for host in /sys/class/scsi_host/*; do
   sudo sh -c "echo min_power > $host/link_power_management_policy"
 done
 
-for device in /sys/bus/i2c/devices/*/device /sys/bus/pci/devices/*; do
-  sudo sh -c "echo auto > $device/power/control"
-done
+sudo sh -c "echo 1 > /sys/module/snd_hda_intel/parameters/power_save"
