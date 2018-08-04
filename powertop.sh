f7f27aa07f95216e25f78d3d7a6845fd7d0eeadb
Piotr Szotkowski <chastell@chastell.net>
2018-08-04 20:24:51 +0200

    Bash: TIL some SCSI hosts do not have that

--- powertop.sh
+++ powertop.sh
@@ -6,8 +6,8 @@ for device in /sys/bus/i2c/devices/*/device /sys/bus/pci/devices/*; do
   sudo sh -c "echo auto > $device/power/control"
 done
 
-for host in /sys/class/scsi_host/*; do
-  sudo sh -c "echo min_power > $host/link_power_management_policy"
+for file in /sys/class/scsi_host/*/link_power_management_policy; do
+  sudo sh -c "echo min_power > $file"
 done
 
 sudo sh -c "echo 1 > /sys/module/snd_hda_intel/parameters/power_save"
