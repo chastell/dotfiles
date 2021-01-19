3ed0dbf64628c0c6fe6627aa9976844ffae4c71b
Piotr Szotkowski <chastell@chastell.net>
2021-01-19 15:39:36 +0100

    Fish: Insta pics: 1080 px on black

--- .config/fish/functions/instagramify.fish
+++ .config/fish/functions/instagramify.fish
@@ -3,9 +3,13 @@ function instagramify
     echo "instagramifying $file"
     set ext  (echo $file | awk -F. '{print $NF}')
     set base (basename $file $ext)
-    magick $file                            \
-      -gravity center                       \
-      -extent "%[fx:h>w?h:w]x%[fx:h>w?h:w]" \
+    magick $file                                 \
+      -gravity     center                        \
+      -background  black                         \
+      -extent      "%[fx:h>w?h:w]x%[fx:h>w?h:w]" \
+      -resize      "1000x1000"                   \
+      -bordercolor black                         \
+      -border      "40x40"                       \
       "$base"instagram.jpg
   end
 end
