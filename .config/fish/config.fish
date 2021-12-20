0ff5937fc1759d4e515ce7db1b920b8fb242abd7
Piotr Szotkowski <chastell@chastell.net>
2021-12-20 20:21:46 +0100

    Fish: reorder config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,8 @@ else
   source /usr/local/opt/asdf/libexec/asdf.fish
 end
 
-starship init fish | source
-
 if test -f /opt/dev/dev.fish
   source /opt/dev/dev.fish
 end
+
+starship init fish | source
