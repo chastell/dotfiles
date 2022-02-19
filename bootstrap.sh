3ed4387b80aeac218e7a5fff187269408800bdee
Piotr Szotkowski <chastell@chastell.net>
2022-02-19 22:34:34 +0100

    bootstrap: install Starship

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,7 @@ ln --force --no-dereference --symbolic --verbose ~/dotfiles/.config/* ~/.config
 
 sudo apt install --assume-yes fish
 
+sudo sh -c "$(curl --fail --location --show-error --silent https://starship.rs/install.sh)" -- --force
+
 # Soon…™
 # sudo chsh --shell /usr/bin/fish "$USER"
