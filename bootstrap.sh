17700d45b0281b90e4acd0200fd8feec66f3c1f7
Piotr Szotkowski <chastell@chastell.net>
2022-08-26 16:50:56 +0200

    bootstrap: package package installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,6 @@ ln --force --no-dereference --symbolic --verbose ~/dotfiles/.gitconfig ~/.gitcon
 curl --create-dirs --fail --location --output ~/.local/share/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall!
 
-sudo apt install --assume-yes fish
-
 sudo sh -c "$(curl --fail --location --show-error --silent https://starship.rs/install.sh)" -- --force
 
 # sudo chsh --shell /usr/bin/fish "$USER"
