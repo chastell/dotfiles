8977d2921ca99a8b41f693e0b8e39dd646425163
Piotr Szotkowski <chastell@chastell.net>
2023-09-06 14:24:34 +0200

    bootstrap: install difftastic

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ ln --force --no-dereference --symbolic --verbose ~/dotfiles/.config/* ~/.config
 ln --force --no-dereference --symbolic --verbose ~/dotfiles/.gitconfig ~/.gitconfig
 ln --force --no-dereference --symbolic --verbose ~/dotfiles/.zshrc ~/.zshrc
 
+wget https://github.com/Wilfred/difftastic/releases/latest/download/difft-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xzf - -C ~/bin
+
 curl --create-dirs --fail --location --output ~/.local/share/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # nvim +PlugInstall +qall!
 
