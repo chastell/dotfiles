d32428f439da91fbb90260c302bc347d51a0edcb
Piotr Szotkowski <chastell@chastell.net>
2024-04-20 12:24:27 +0200

    bootstrap: alias bat & fd on Ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,9 @@ ln --force --no-dereference --symbolic --verbose ~/dotfiles/.gitconfig ~/.gitcon
 ln --force --no-dereference --symbolic --verbose ~/dotfiles/.zshrc ~/.zshrc
 ln --force --no-dereference --symbolic --verbose ~/dotfiles/bin ~/bin
 
+ln --force --no-dereference --symbolic --verbose /usr/bin/batcat ~/bin/bat
+ln --force --no-dereference --symbolic --verbose /usr/bin/fdfind ~/bin/fd
+
 wget https://github.com/Wilfred/difftastic/releases/latest/download/difft-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xzf - -C ~/bin
 
 curl --create-dirs --fail --location --output ~/.local/share/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
