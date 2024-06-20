6ce1471b521e0e19ea77d3a77b4d7308a39e2ce6
Piotr Szotkowski <chastell@chastell.net>
2024-06-20 13:28:18 +0200

    bootstrap: install stuff in parallel

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,9 +10,10 @@ ln --force --no-dereference --symbolic --verbose ~/dotfiles/bin ~/bin
 ln --force --no-dereference --symbolic --verbose /usr/bin/batcat ~/bin/bat
 ln --force --no-dereference --symbolic --verbose /usr/bin/fdfind ~/bin/fd
 
-wget https://github.com/Wilfred/difftastic/releases/latest/download/difft-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xzf - -C ~/bin
-wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xzf - -C ~/bin
-wget https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz -O - | tar -xzf - -C ~/bin
+wget https://github.com/Wilfred/difftastic/releases/latest/download/difft-x86_64-unknown-linux-gnu.tar.gz   -O - | tar -xzf - -C ~/bin &
+wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xzf - -C ~/bin &
+wget https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz  -O - | tar -xzf - -C ~/bin &
+wait
 
 curl --create-dirs --fail --location --output ~/.local/share/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # nvim +PlugInstall +qall!
