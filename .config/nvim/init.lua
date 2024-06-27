7525dce13da426ab2a1e92bffd7497c6602ca789
Piotr Szotkowski <chastell@chastell.net>
2024-06-27 20:59:03 +0200

    Neovim: configure wildmode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,6 +11,7 @@ vim.g.have_nerd_font = true
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
+vim.opt.wildmode = 'list:longest'
 
 -- Make line numbers default
 vim.opt.number = true
