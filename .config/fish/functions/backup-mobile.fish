e4cd824143ece2ff01f6bdb737955c7553166f3f
Piotr Szotkowski <chastell@chastell.net>
2024-07-05 06:43:25 +0200

    Fish: backup more of mobile

--- .config/fish/functions/backup-mobile.fish
+++ .config/fish/functions/backup-mobile.fish
@@ -1,25 +1,14 @@
 function backup-mobile
-  rsync                                            \
-    --archive                                      \
-    --delete                                       \
-    --delete-excluded                              \
-    --exclude=.thumbnails                          \
-    --exclude=/Android/data                        \
-    --exclude=/Android/obb                         \
-    --exclude=Audiobooks                           \
-    --exclude=Client\ for\ Put.io                  \
-    --exclude=cache                                \
-    --exclude=com.babbel.mobile.android.en         \
-    --exclude=com.ea.game.starwarscapital_row      \
-    --exclude=com.google.android.apps.youtube.kids \
-    --exclude=com.netflix.mediaclient              \
-    --exclude=com.sagosago.World.googleplay        \
-    --exclude=com.spotify.music                    \
-    --exclude=podcasts                             \
-    --ignore-errors                                \
-    --no-i-r                                       \
-    --partial                                      \
-    --progress                                     \
-    --rsh="ssh -p 2222"                            \
-    admin@$argv:SDCard/ ~/A70/SDCard/
+  rsync                     \
+    --archive               \
+    --delete                \
+    --delete-excluded       \
+    --exclude=/Android/data \
+    --exclude=/Android/obb  \
+    --ignore-errors         \
+    --no-i-r                \
+    --partial               \
+    --progress              \
+    --rsh="ssh -p 2222"     \
+    admin@$argv:SDCard/ ~/Xperia/SDCard/
 end
