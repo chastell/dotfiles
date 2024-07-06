24eabcdd8946e84a1bc97e1757e4ca1cc4e7ce7f
Piotr Szotkowski <chastell@chastell.net>
2024-07-06 11:01:06 +0200

    Fish: auto start Zellij

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,8 @@ end
 
 shadowenv init fish | source
 starship init fish | source
+
+if status is-interactive
+  set ZELLIJ_AUTO_EXIT true
+  eval (zellij setup --generate-auto-start fish | string collect)
+end
