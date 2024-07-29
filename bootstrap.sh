85674bc785b1b4a3616af2234a1e1d18f281993c
Piotr Szotkowski <chastell@chastell.net>
2024-07-30 00:48:44 +0200

    bootstrap: refactor binary installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,10 +10,15 @@ ln --force --no-dereference --symbolic --verbose ~/dotfiles/bin ~/bin
 ln --force --no-dereference --symbolic --verbose /usr/bin/batcat ~/bin/bat
 ln --force --no-dereference --symbolic --verbose /usr/bin/fdfind ~/bin/fd
 
-wget https://github.com/Wilfred/difftastic/releases/latest/download/difft-x86_64-unknown-linux-gnu.tar.gz   -O - | tar -xzf - -C ~/bin &
-wget https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_0.43.1_Linux_x86_64.tar.gz   -O - | tar -xzf - -C ~/bin &
-wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xzf - -C ~/bin &
-wget https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz  -O - | tar -xzf - -C ~/bin &
+urls="
+  https://github.com/Wilfred/difftastic/releases/latest/download/difft-x86_64-unknown-linux-gnu.tar.gz
+  https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_0.43.1_Linux_x86_64.tar.gz
+  https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz
+  https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
+"
+for url in $urls; do
+  wget $url -O - | tar -xzf - -C ~/bin &
+done
 wait
 
 sudo chsh --shell /usr/bin/fish "$USER"
