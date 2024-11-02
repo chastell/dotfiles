24e814cdfbd7e0364e368cdfccaf3c8ffd65b7f7
Piotr Szotkowski <chastell@chastell.net>
2024-11-02 15:07:19 +0100

    bootstrap: upgrade lazygit

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ ln --force --no-dereference --symbolic --verbose /usr/bin/fdfind ~/bin/fd
 
 urls="
   https://github.com/Wilfred/difftastic/releases/latest/download/difft-x86_64-unknown-linux-gnu.tar.gz
-  https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_0.43.1_Linux_x86_64.tar.gz
+  https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_0.44.1_Linux_x86_64.tar.gz
   https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz
   https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
 "
