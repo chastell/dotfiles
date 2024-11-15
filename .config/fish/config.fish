6d1103ef813e4eaa495b863dc5382f3108cc122c
Piotr Szotkowski <chastell@chastell.net>
2024-11-15 16:09:09 +0100

    Fish: drop asdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,6 @@ set --export --universal EDITOR nvim
 
 fish_add_path ~/bin ~/.cargo/bin /usr/local/sbin /opt/homebrew/sbin /opt/homebrew/bin
 
-if test -f ~/.asdf/asdf.fish
-  source ~/.asdf/asdf.fish
-else if test -f /opt/homebrew/Cellar/asdf/0.9.0/libexec/asdf.fish
-  source /opt/homebrew/Cellar/asdf/0.9.0/libexec/asdf.fish
-end
-
 if test -f /opt/dev/dev.fish
   source /opt/dev/dev.fish
 end
