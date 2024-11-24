1e546190e80baac83b069984194106a400fe0c70
Piotr Szotkowski <chastell@chastell.net>
2024-11-24 22:39:36 +0100

    Neovim: never too sure how much nesting this needs

--- .config/nvim/lua/custom/plugins/lazygit.lua
+++ .config/nvim/lua/custom/plugins/lazygit.lua
@@ -1,14 +1,16 @@
 return {
-  'kdheepak/lazygit.nvim',
-  cmd = {
-    'LazyGit',
-    'LazyGitConfig',
-    'LazyGitCurrentFile',
-    'LazyGitFilter',
-    'LazyGitFilterCurrentFile',
-  },
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = {
-    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
 }
