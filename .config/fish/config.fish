2487ae619d5efb46bfc46aef86b2b91e446b7a76
Piotr Szotkowski <chastell@chastell.net>
2025-02-05 19:09:42 +0100

    Fish: don’t start Zellij by default

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,8 +15,3 @@ end
 if type -q starship
   starship init fish | source
 end
-
-if status is-interactive
-  set ZELLIJ_AUTO_EXIT true
-  eval (zellij setup --generate-auto-start fish | string collect)
-end
