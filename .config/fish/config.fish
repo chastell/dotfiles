a59a961c962cba238ea41c72b7b2b544cfb88ebf
Piotr Szotkowski <chastell@chastell.net>
2025-05-21 15:43:13 +0200

    Fish: Helix wins, indentation adjusted

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,25 +5,25 @@ set fish_greeting
 fish_add_path ~/bin ~/.cargo/bin /usr/local/sbin /opt/homebrew/sbin /opt/homebrew/bin
 
 if test -f /opt/dev/dev.fish
-  source /opt/dev/dev.fish
+    source /opt/dev/dev.fish
 end
 
 if type -q shadowenv
-  shadowenv init fish | source
+    shadowenv init fish | source
 end
 
 if type -q starship
-  starship init fish | source
+    starship init fish | source
 end
 
 if not type -q hx; and type -q helix
-  alias hx helix
+    alias hx helix
 end
 
 if not type -q xdg-open; and type -q open
-  alias xdg-open open
+    alias xdg-open open
 end
 
 if not type -q zed; and type -q zeditor
-  alias zed zeditor
+    alias zed zeditor
 end
